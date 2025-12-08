f1124cad1fb727f828bcf958b1caed8887bdba66
Mohammed Hafiz <mohammed.hafiz.475@gmail.com>
2025-12-08 16:50:48 +0530

    added missing animation to arc reactor

--- src/components/ArcReactorModel/ArcReactorModel.jsx
+++ src/components/ArcReactorModel/ArcReactorModel.jsx
@@ -1,16 +1,40 @@
 // src/components/ArcReactorModel/ArcReactorModel.jsx
-import React from "react";
+
+import React, { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function ArcReactorModel({
     position = [0, 0, 0],
     rotation = [0, 0, 0],
     scale = 1,
 }) {
-    const { scene } = useGLTF("/models/primary-ion-drive/primary_ion_drive.glb");
+    const group = useRef();
+    const { scene, animations } = useGLTF("/models/primary-ion-drive/primary_ion_drive.glb");
+
+    const mixer = useRef(null);
+
+    // PLAY ORIGINAL ANIMATIONS FROM MODEL
+    useEffect(() => {
+        if (animations && animations.length > 0) {
+            mixer.current = new THREE.AnimationMixer(scene);
+
+            animations.forEach((clip) => {
+                const action = mixer.current.clipAction(clip);
+                action.play();
+            });
+
+            console.log("🎬 Playing GLB animations:", animations);
+        }
+    }, [scene, animations]);
+
+    useFrame((_, delta) => {
+        if (mixer.current) mixer.current.update(delta);
+    });
 
     return (
-        <group position={position} rotation={rotation} scale={scale}>
+        <group ref={group} position={position} rotation={rotation} scale={scale}>
             <primitive object={scene} />
         </group>
     );
